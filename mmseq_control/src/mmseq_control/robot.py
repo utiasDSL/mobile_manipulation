ead90df651c72146d99d04edae1b98350d342133
Jingxing Qian <jingxing.qian@mail.utoronto.ca>
2024-09-09 13:14:56 -0400

    wip planner integration

--- mmseq_control/src/mmseq_control/robot.py
+++ mmseq_control/src/mmseq_control/robot.py
@@ -19,7 +19,6 @@ import mmseq_control.map as map
 from mmseq_utils import parsing
 from mmseq_simulator import simulation
 from mobile_manipulation_central.kinematics import RobotKinematics
-from cbf_mpc.barrier_function2 import CBF, CBFJacobian
 
 # import yappi
 
