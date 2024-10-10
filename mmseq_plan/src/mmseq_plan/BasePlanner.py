9a49bd358a18c6c87ce4b775a33ddf0527d0c1a5
Jingxing Qian <jingxing.qian@mail.utoronto.ca>
2024-10-10 16:05:08 -0400

    exp tuning

--- mmseq_plan/src/mmseq_plan/BasePlanner.py
+++ mmseq_plan/src/mmseq_plan/BasePlanner.py
@@ -540,7 +540,7 @@ class ROSTrajectoryPlanner(TrajectoryPlanner):
 
         poses = np.hstack((new_x, new_y, new_desired_headings))
 
-        self.close_to_finish = (poses[-1,:] == poses[-2,:]).all() and np.linalg.norm(self.robot_states[0][:2] - poses[-1,:2]) < 0.1 and np.abs(wrap_pi_scalar(self.robot_states[0][2] - poses[-1,2])) < 0.1
+        self.close_to_finish = self.robot_states and (poses[-1,:] == poses[-2,:]).all() and np.linalg.norm(self.robot_states[0][:2] - poses[-1,:2]) < 0.1 and np.abs(wrap_pi_scalar(self.robot_states[0][2] - poses[-1,2])) < 0.1
 
         return {'t': time,'s': time * self.cruise_speed, 'p': poses, 'v': velocities}
 
