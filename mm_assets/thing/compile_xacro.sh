01312a5c75716abd3c3ebcd666f51d3ef35555d5
Federico-PizarroBejarano <federico.pizarrobejarano@mail.utoronto.ca>
2025-12-11 15:21:37 -0500

    Cleaning up mm_assets

--- mm_assets/thing/compile_xacro.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Compile xacro versions of URDFs to regular versions for consumption by libraries.
-# This is mainly used to resolve ROS package paths without hardcoding.
-mkdir -p urdf
-xacro xacro/test.urdf.xacro -o urdf/test.urdf
